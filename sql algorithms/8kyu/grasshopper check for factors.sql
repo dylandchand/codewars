2680a5671630a6b3b4e6a13b870ede257460c08d
ddc465 <dylancbusinessenq@gmail.com>
2023-08-28 03:19:09 +1000

    added grasshopper check for factors .sql

---
+++ sql algorithms/8kyu/grasshopper check for factors.sql
@@ -0,0 +1,12 @@
+/* 
+
+This function should test if the factor is a factor of base.
+
+Return true if it is a factor or false if it is not. 
+
+-- you will be given a table 'kata' with columns 'id', 'base', and 'factor'. 
+-- return the 'id' and your result in a column named 'res'.
+
+*/
+
+SELECT id, base % factor = 0 as res FROM kata
