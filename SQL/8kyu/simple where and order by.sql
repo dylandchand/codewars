f3a32980561584559e32dbcac19e5a56716ee27f
ddc465 <dylancbusinessenq@gmail.com>
2023-11-01 07:27:42 +1000

    added file simple where and order by.sql

---
+++ SQL/8kyu/simple where and order by.sql
@@ -0,0 +1,9 @@
+/*
+people table schema
+id
+name
+age
+You should return all people fields where their age is over 50 and order by the age descending
+*/
+SELECT * FROM people WHERE age > 50
+ORDER BY age DESC;
